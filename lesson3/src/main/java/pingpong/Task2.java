505359dc4495b910fa40c290eb05f2812da882bc
OldBrain <hell_brain@mail.ru>
2022-01-27 20:59:13 +0400

    ДЗ №3

---
+++ lesson3/src/main/java/pingpong/Task2.java
@@ -0,0 +1,40 @@
+package pingpong;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Task2 {
+    private int counter;
+    private Lock lock = new ReentrantLock(true);
+
+    public void runTask() {
+
+new Thread(()->{
+    for (int i = 0; i < 5; i++) {
+        lock.lock();
+        counter++;
+        System.out.println(counter);
+        lock.unlock();
+    }
+}).start();
+
+new Thread(()->{
+    for (int i = 0; i < 5; i++) {
+        lock.lock();
+        counter++;
+        System.out.println(counter);
+        lock.unlock();
+    }
+}).start();
+
+new Thread(()->{
+    for (int i = 0; i < 5; i++) {
+        lock.lock();
+        counter++;
+        System.out.println(counter);
+        lock.unlock();
+    }
+}).start();
+    }
+
+}
